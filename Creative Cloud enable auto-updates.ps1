424c5e55c2cacc2b7de446d15a315005abe4099b
SONiQz <adrian.forrester.17@googlemail.com>
2025-07-16 11:51:09 +0100

    Add files via upload

---
+++ Creative Cloud enable auto-updates.ps1
@@ -0,0 +1,5 @@
+﻿$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$Source = "$PSScriptRoot\com.adobe.acc.container.default.prefs"
+$Destination = "$env:LOCALAPPDATA\Adobe\OOBE\"
+
+Copy-Item -Path $Source -Destination $Destination –Recurse -Force
